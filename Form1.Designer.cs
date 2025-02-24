15ff218dd3f84d05c464595770100ad16ef4c5b7
shipclone06 <anhkhoa0309@gmail.com>
2025-02-24 11:21:33 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace bai13_canchi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btntinh = new System.Windows.Forms.Button();
+            this.btnexit = new System.Windows.Forms.Button();
+            this.nam = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtcanchi = new System.Windows.Forms.TextBox();
+            this.txtnam = new System.Windows.Forms.TextBox();
+            this.lstnam = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btntinh
+            // 
+            this.btntinh.Location = new System.Drawing.Point(221, 77);
+            this.btntinh.Name = "btntinh";
+            this.btntinh.Size = new System.Drawing.Size(92, 49);
+            this.btntinh.TabIndex = 0;
+            this.btntinh.Text = "tinh";
+            this.btntinh.UseVisualStyleBackColor = true;
+            this.btntinh.Click += new System.EventHandler(this.btntinh_Click);
+            // 
+            // btnexit
+            // 
+            this.btnexit.Location = new System.Drawing.Point(221, 146);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(92, 46);
+            this.btnexit.TabIndex = 0;
+            this.btnexit.Text = "exit";
+            this.btnexit.UseVisualStyleBackColor = true;
+            this.btnexit.Click += new System.EventHandler(this.btnexit_Click);
+            // 
+            // nam
+            // 
+            this.nam.AutoSize = true;
+            this.nam.Location = new System.Drawing.Point(12, 77);
+            this.nam.Name = "nam";
+            this.nam.Size = new System.Drawing.Size(29, 13);
+            this.nam.TabIndex = 1;
+            this.nam.Text = "Năm";
+            this.nam.Click += new System.EventHandler(this.nam_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "căn chi";
+            // 
+            // txtcanchi
+            // 
+            this.txtcanchi.Location = new System.Drawing.Point(86, 146);
+            this.txtcanchi.Name = "txtcanchi";
+            this.txtcanchi.Size = new System.Drawing.Size(100, 20);
+            this.txtcanchi.TabIndex = 2;
+            // 
+            // txtnam
+            // 
+            this.txtnam.Location = new System.Drawing.Point(86, 77);
+            this.txtnam.Name = "txtnam";
+            this.txtnam.Size = new System.Drawing.Size(100, 20);
+            this.txtnam.TabIndex = 2;
+            // 
+            // lstnam
+            // 
+            this.lstnam.FormattingEnabled = true;
+            this.lstnam.Location = new System.Drawing.Point(343, 77);
+            this.lstnam.Name = "lstnam";
+            this.lstnam.Size = new System.Drawing.Size(122, 160);
+            this.lstnam.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstnam);
+            this.Controls.Add(this.txtnam);
+            this.Controls.Add(this.txtcanchi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nam);
+            this.Controls.Add(this.btnexit);
+            this.Controls.Add(this.btntinh);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btntinh;
+        private System.Windows.Forms.Button btnexit;
+        private System.Windows.Forms.Label nam;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtcanchi;
+        private System.Windows.Forms.TextBox txtnam;
+        private System.Windows.Forms.ListBox lstnam;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai13_canchi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btntinh_Click(object sender, EventArgs e)
+        {
+            string[] canArr = { "Canh", "Tan", "Nham", "Quy", "Giap", "At", "Binh", "Dinh", "Mau", "Ki" };
+            string[] chiArr = { "Than", "Dau", "Tuat", "Hoi", "Ty", " Suu", "Dan", "Mao", "Thin", "Ty", "Ngon", "Mui" };
+
+            string giatritrave = canArr[Convert.ToInt32(txtnam.Text) % 10] + " " + chiArr[Convert.ToInt32(txtnam.Text) % 12];
+            txtcanchi.Text = giatritrave;
+        }
+
+        private void btnexit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           for (int i = 2000; i <= 2050; i++) {
+                lstnam.Items.Add(i+ "-" + );
+        }
+    }
+
+        private void nam_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
