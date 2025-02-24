d0cdae2572c78b99687836c7a5d6ece9646b8e25
shipclone06 <anhkhoa030900@gmail.com>
2025-02-24 13:43:56 +0700

    update1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -108,7 +108,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightBlue;
-            this.ClientSize = new System.Drawing.Size(408, 219);
+            this.ClientSize = new System.Drawing.Size(408, 211);
             this.Controls.Add(this.lstnam);
             this.Controls.Add(this.txtnam);
             this.Controls.Add(this.txtcanchi);
